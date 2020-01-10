448df3b73561f8a92faf17940241ae57110b77bc
Shawn Place <shawnplace11@gmail.com>
2020-01-10 16:01:36 -0500

    Initial commit with files

---
+++ Arrays.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLibrary
+{
+    public class Arrays
+    {
+        public int[] arr;
+        public int len;
+        private int indexSpot = 0;
+        public int orgLen;
+
+        public Arrays(int size)
+        {
+            orgLen = size;
+            len = size;
+            arr = new int[len];
+        }
+
+        public int[] insert(int num)
+        {
+            if (indexSpot < orgLen)
+            {
+                arr[indexSpot] = num;
+                indexSpot++;
+            }
+            else
+            {
+                len++;
+                int[] newArr = new int[len];
+                for (int i = 0; i < len - 1; i++)
+                {
+                    newArr[i] = arr[i];
+                }
+                newArr[len - 1] = num;
+                arr = newArr;
+            }
+
+            return arr;
+        }
+
+        public int[] removeAt(int num)
+        {
+            int j = 0;
+            len--;
+            int[] newArr = new int[len];
+            for(int i = 0 ; i < len; i++)
+            {
+                if(arr[j] != arr[num])
+                {
+                    newArr[i] = arr[j];
+                    j++;
+                }
+                else
+                {
+                    j++;
+                    newArr[i] = arr[j];
+                    j++;
+                }
+            }
+
+            arr = newArr;
+
+            return arr;
+        }
+
+        public void print()
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void printIndex()
+        {
+            Console.WriteLine(indexSpot);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arrays number = new Arrays(3);
+
+            number.insert(0);
+            number.insert(1);
+            number.insert(2);
+            number.insert(3);
+            number.insert(4);
+            number.print();
+
+            number.removeAt(1);
+            number.print();
+
+            number.removeAt(2);
+            number.print();
+
+            Console.ReadLine();
+        }
+    }
+}
